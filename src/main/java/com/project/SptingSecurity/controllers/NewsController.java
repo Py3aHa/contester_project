5198c7e62b13bf575149846837f569a235031ddb
Ruzana <Py3aHa99@gmail.com>
2020-03-28 13:42:46 +0600

    add news page

---
+++ src/main/java/com/project/SptingSecurity/controllers/NewsController.java
@@ -0,0 +1,30 @@
+package com.project.SptingSecurity.controllers;
+
+import com.project.SptingSecurity.Repositories.NewPostsRepositories;
+import com.project.SptingSecurity.Repositories.RolesRepositories;
+import com.project.SptingSecurity.Repositories.UserRepositories;
+import com.project.SptingSecurity.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NewsController {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRepositories userRepositories;
+
+    @Autowired
+    RolesRepositories rolesRepositories;
+
+    @Autowired
+    NewPostsRepositories newPostsRepositories;
+
+    @GetMapping(path = "/addNewPost")
+    public String addNewPost(){
+        return "addNewPost";
+    }
+}
